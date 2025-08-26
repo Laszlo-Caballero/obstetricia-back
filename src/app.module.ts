3ae04d8f21ba39eddabecc02546ebb4b769273b8
laszlo-caballero <laszlocaballero@gmail.com>
2025-08-26 15:12:39 -0500

    feat: initialize NestJS application with TypeORM and MSSQL configuration
    - Created app.module.ts to set up global configuration and database connection.
    - Added main.ts to bootstrap the NestJS application.
    - Configured TypeScript build settings in tsconfig.build.json.
    - Established TypeScript compiler options in tsconfig.json.
    - Implemented typeorm.config.ts for database connection settings using environment variables.

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      options: {
+        encrypt: process.env.ENCRYPT === 'true',
+        trustServerCertificate: true,
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
