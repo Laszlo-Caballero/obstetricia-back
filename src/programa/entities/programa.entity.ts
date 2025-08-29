091a013057b58aa3394e38147ebfa0fb083c631b
luciana-31 <lgogny74@gmail.com>
2025-08-29 00:13:34 -0500

    feat: Atributos de la entidad Programa

--- src/programa/entities/programa.entity.ts
+++ src/programa/entities/programa.entity.ts
@@ -1,1 +1,21 @@
-export class Programa {}
+import {
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Programa {
+    @PrimaryGeneratedColumn()
+    programaId: number;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    descripcion: string;
+
+    @Column({ default: true })
+    estado: boolean;
+
+}
